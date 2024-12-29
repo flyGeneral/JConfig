ae0c368a3e62fd8e08b3f275fcdf8c90a5d07e3c
chenjinghao <chenjinghao@cvte.com>
2024-12-29 13:07:10 +0800

    [nvim][git] add git plugin

--- nvim/lua/plugins/plugins-setup.lua
+++ nvim/lua/plugins/plugins-setup.lua
@@ -83,7 +83,24 @@ require("lazy").setup({
         "numToStr/Comment.nvim",
         event = "VeryLazy",
     },
-
+    -- git plugin
+    {
+        event = "VeryLazy",
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    },
+    {
+        event = "VeryLazy",
+        "tpope/vim-fugitive",
+        cmd = "Git",
+        config = function()
+            -- convert
+            vim.cmd.cnoreabbrev([[git Git]])
+            vim.cmd.cnoreabbrev([[gp Git push]])
+        end,
+    },
 })
 
 require("mason").setup()
